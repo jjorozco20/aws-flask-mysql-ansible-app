f948501d53482e57afcb8ea85bce0b86e5a09ca4
Juan Orozco Serrano <juan.orozco@jalasoft.com>
2024-11-28 10:17:29 -0600

    Added new functionality to work with docker

--- terraform/codedeploy_iam.tf
+++ terraform/codedeploy_iam.tf
@@ -1,22 +1,22 @@
-# Define the IAM Role for CodeDeploy
-resource "aws_iam_role" "codedeploy_role" {
-  name               = "codedeploy-role"
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "codedeploy.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  })
-}
+# # Define the IAM Role for CodeDeploy
+# resource "aws_iam_role" "codedeploy_role" {
+#   name               = "codedeploy-role"
+#   assume_role_policy = jsonencode({
+#     "Version": "2012-10-17",
+#     "Statement": [
+#       {
+#         "Effect": "Allow",
+#         "Principal": {
+#           "Service": "codedeploy.amazonaws.com"
+#         },
+#         "Action": "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
 
-# Attach the AWSCodeDeployRole policy to the role
-resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
-  role       = aws_iam_role.codedeploy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
-}
+# # Attach the AWSCodeDeployRole policy to the role
+# resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
+#   role       = aws_iam_role.codedeploy_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+# }
--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -24,13 +24,15 @@ resource "aws_key_pair" "app_key" {
 
 # EC2 Instances
 resource "aws_instance" "app" {
-  count           = var.instaces_number
+  count           = 1
   ami             = data.aws_ami.amazon_linux.id
   instance_type   = var.ec2_instance_type
   subnet_id       = aws_subnet.public_subnet_1.id
   security_groups = [aws_security_group.ec2_public_sg.id]
   key_name        = aws_key_pair.app_key.key_name
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  depends_on = [aws_db_instance.mysql]
 
   tags = {
     Name = "app-server-${count.index + 1}"
@@ -47,9 +49,10 @@ resource "aws_instance" "app" {
   provisioner "local-exec" {
     command = <<-EOT
       docker run --rm `
-        -v "${path.cwd}/provisioning/deploy_flask_app.yml:/ansible/playbook.yml" `
+        -v "${path.cwd}/provisioning/deploy_docker.yml:/ansible/playbook.yml" `
         -v "${path.cwd}/ssh_key:/root/.ssh/id_rsa" `
         -e ANSIBLE_HOST_KEY_CHECKING=False `
+        -e DB_ENDPOINT="${aws_db_instance.mysql.endpoint}" `
         williamyeh/ansible:alpine3 `
         sh -c "chmod 600 /root/.ssh/id_rsa && ansible-playbook -i '${self.public_ip},' /ansible/playbook.yml -u ec2-user"
     EOT
@@ -64,55 +67,43 @@ resource "aws_instance" "app" {
   }
 }
 
-resource "aws_instance" "load_balancer" {
-  ami             = data.aws_ami.amazon_linux.id
-  instance_type   = var.ec2_instance_type
-  subnet_id       = aws_subnet.public_subnet_1.id
-  security_groups = [aws_security_group.ec2_public_sg.id]
-  key_name        = aws_key_pair.app_key.key_name
-  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+# resource "aws_instance" "load_balancer" {
+#   ami             = data.aws_ami.amazon_linux.id
+#   instance_type   = var.ec2_instance_type
+#   subnet_id       = aws_subnet.public_subnet_1.id
+#   security_groups = [aws_security_group.ec2_public_sg.id]
+#   key_name        = aws_key_pair.app_key.key_name
+#   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
-  tags = {
-    Name = "load-balancer"
-    Role = "LoadBalancer"
-  }
+#   tags = {
+#     Name = "load-balancer"
+#     Role = "LoadBalancer"
+#   }
 
-  lifecycle {
-    ignore_changes = [
-      security_groups,
-      ami
-    ]
-  }
+#   lifecycle {
+#     ignore_changes = [
+#       security_groups,
+#       ami
+#     ]
+#   }
 
-  provisioner "local-exec" {
-    command = <<-EOT
-      docker run --rm `
-        -v "${path.cwd}/provisioning/deploy_loadbalancer.yml:/ansible/playbook.yml" `
-        -v "${path.cwd}/ssh_key:/root/.ssh/id_rsa" `
-        -e ANSIBLE_HOST_KEY_CHECKING=False `
-        -e APP_SERVER_IPS='${join(",", aws_instance.app[*].public_ip)}' `
-        williamyeh/ansible:alpine3 `
-        sh -c "chmod 600 /root/.ssh/id_rsa && ansible-playbook -i '${self.public_ip},' /ansible/playbook.yml -u ec2-user"
-    EOT
-    interpreter = ["powershell", "-Command"]
-  }
+#   provisioner "local-exec" {
+#     command = <<-EOT
+#       docker run --rm `
+#         -v "${path.cwd}/provisioning/deploy_loadbalancer.yml:/ansible/playbook.yml" `
+#         -v "${path.cwd}/ssh_key:/root/.ssh/id_rsa" `
+#         -e ANSIBLE_HOST_KEY_CHECKING=False `
+#         -e APP_SERVER_IPS='${join(",", aws_instance.app[*].public_ip)}' `
+#         williamyeh/ansible:alpine3 `
+#         sh -c "chmod 600 /root/.ssh/id_rsa && ansible-playbook -i '${self.public_ip},' /ansible/playbook.yml -u ec2-user"
+#     EOT
+#     interpreter = ["powershell", "-Command"]
+#   }
 
-  connection {
-    type        = "ssh"
-    user        = var.ec2_user
-    private_key = tls_private_key.ssh_key.private_key_pem
-    host        = self.public_ip
-  }
-}
-
-
-resource "aws_eip" "app_eip" {
-  count    = 2
-  instance = aws_instance.app[count.index].id
-  depends_on = [aws_instance.app]
-}
-
-resource "aws_eip" "load_balancer_eip" {
-  instance = aws_instance.load_balancer.id
-  depends_on = [aws_instance.load_balancer]
-}
+#   connection {
+#     type        = "ssh"
+#     user        = var.ec2_user
+#     private_key = tls_private_key.ssh_key.private_key_pem
+#     host        = self.public_ip
+#   }
+# }
